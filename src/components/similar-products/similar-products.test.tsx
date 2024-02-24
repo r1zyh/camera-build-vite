52f66c8424d3c7b324f8eef4d3f3573f3fe0e870
r1zyh <kirill.ananiev2012@yandex.ru>
2024-02-24 15:07:39 +0300

    Добавит тесты для SimilarProducts

---
+++ src/components/similar-products/similar-products.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import { makeFakeProduct } from '../../store/mock-components/mocks';
+import SimilarProducts from './similar-products';
+import { withHistory } from '../../store/mock-components/mock-components';
+import userEvent from '@testing-library/user-event';
+
+describe('SimilarProducts', () => {
+  const fakeProducts = [makeFakeProduct(), makeFakeProduct(),makeFakeProduct(), makeFakeProduct() ];
+  it('renders similar products correctly', () => {
+    render(withHistory(<SimilarProducts similarProducts={fakeProducts} />));
+
+    const productCards = screen.getAllByTestId('product-card-div');
+    expect(productCards.length).toBe(fakeProducts.length);
+  });
+
+  it('disables prev button on first page', () => {
+    render(withHistory(<SimilarProducts similarProducts={fakeProducts} />));
+
+    const prevButton = screen.getByLabelText('Предыдущая страница');
+    expect(prevButton).toBeDisabled();
+  });
+
+  it('enables next button when there are more pages', () => {
+    render(withHistory(<SimilarProducts similarProducts={fakeProducts} />));
+
+    const nextButton = screen.getByLabelText('Следующая страница');
+    expect(nextButton).toBeEnabled();
+  });
+
+  it('renders the next page of products when next button is clicked', () => {
+    render(withHistory(<SimilarProducts similarProducts={fakeProducts} />));
+
+    userEvent.click(screen.getByLabelText('Следующая страница'));
+    const visibleProducts = screen.getAllByTestId('product-card-div').filter((product) => product.classList.contains('is-active'));
+    expect(visibleProducts.length).toBeLessThanOrEqual(3);
+  });
+
+  it('renders the prev page of products when prev button is clicked', () => {
+    render(withHistory(<SimilarProducts similarProducts={fakeProducts} />));
+
+    userEvent.click(screen.getByLabelText('Следующая страница'));
+    userEvent.click(screen.getByLabelText('Предыдущая страница'));
+    const visibleProducts = screen.getAllByTestId('product-card-div').filter((product) => product.classList.contains('is-active'));
+    expect(visibleProducts.length).toBeLessThanOrEqual(3);
+  });
+});
