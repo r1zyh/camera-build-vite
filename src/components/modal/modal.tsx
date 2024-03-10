c1457f897c5b27b0e1cee2cb962a66e29e349079
r1zyh <kirill.ananiev2012@yandex.ru>
2024-03-10 22:27:00 +0300

    Зациклит Tab в модальных окнах

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useCallback, useEffect } from 'react';
+import { ReactNode, useCallback, useEffect, useRef } from 'react';
 
 type ModalProps = {
   title: string;
@@ -13,23 +13,45 @@ function Modal({
   buttons,
   closeModal,
 }: ModalProps): JSX.Element {
+  const modalRef = useRef<HTMLDivElement>(null);
+  const overlayRef = useRef<HTMLDivElement>(null);
+  const firstFocusableElementRef = useRef<HTMLElement | null>(null);
+  const lastFocusableElementRef = useRef<HTMLElement | null>(null);
+
   const handleCloseModal = useCallback(() => {
     closeModal();
   }, [closeModal]);
 
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    const handleKeyDown = (e: KeyboardEvent) => {
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
       if (e.key === 'Escape') {
         handleCloseModal();
       }
-    };
+
+      if (e.key === 'Tab') {
+        if (
+          e.shiftKey &&
+          document.activeElement === firstFocusableElementRef.current
+        ) {
+          e.preventDefault();
+          lastFocusableElementRef.current?.focus();
+        } else if (
+          !e.shiftKey &&
+          document.activeElement === lastFocusableElementRef.current
+        ) {
+          e.preventDefault();
+          firstFocusableElementRef.current?.focus();
+        }
+      }
+    },
+    [handleCloseModal]
+  );
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
 
     const handleClickOutside = (e: MouseEvent) => {
-      if (
-        e.target instanceof HTMLElement &&
-        !e.target.closest('.modal__content')
-      ) {
+      if (overlayRef.current && e.target === overlayRef.current) {
         handleCloseModal();
       }
     };
@@ -42,12 +64,32 @@ function Modal({
       document.removeEventListener('keydown', handleKeyDown);
       document.removeEventListener('click', handleClickOutside);
     };
-  }, [closeModal, handleCloseModal]);
+  }, [closeModal, handleCloseModal, handleKeyDown]);
+
+  useEffect(() => {
+    if (modalRef.current) {
+      const focusableElements = Array.from(
+        modalRef.current?.querySelectorAll('input, button, [tabindex="0"]') ||
+          []
+      );
+      firstFocusableElementRef.current =
+        focusableElements[0] as HTMLElement | null;
+      lastFocusableElementRef.current = focusableElements[
+        focusableElements.length - 1
+      ] as HTMLElement | null;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (modalRef.current && firstFocusableElementRef.current) {
+      firstFocusableElementRef.current.focus();
+    }
+  }, []);
 
   return (
-    <div className="modal is-active" data-testid="modal">
+    <div className="modal is-active" data-testid="modal" ref={modalRef}>
       <div className="modal__wrapper">
-        <div className="modal__overlay"></div>
+        <div className="modal__overlay" ref={overlayRef}></div>
         <div className="modal__content">
           <p className="title title--h4">{title}</p>
           {content}
@@ -56,7 +98,7 @@ function Modal({
             className="cross-btn"
             type="button"
             aria-label="Закрыть попап"
-            onClick={handleCloseModal}
+            onClick={closeModal}
           >
             <svg width="10" height="10" aria-hidden="true">
               <use xlinkHref="#icon-close"></use>
